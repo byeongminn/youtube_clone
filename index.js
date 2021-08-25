b7d24f15470b3ed012866be28d7958e34e6d911b
DESKTOP-TVPJ660\user <sekn1997@naver.com>
2021-08-25 11:48:38 +0900

    회원가입

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require("mongoose");
 const config = require("./server/config/key");
 
+const { User } = require("./server/models/User");
+
 const app = express();
 mongoose.connect(config.mongoURI, {
     useNewUrlParser: true,
@@ -14,6 +16,17 @@ app.use(express.json());
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+app.post("/api/users/register", (req, res) => {
+    const user = User(req.body);
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err });
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(5000, function () {
   console.log('Example app listening on http://localhost:5000');
