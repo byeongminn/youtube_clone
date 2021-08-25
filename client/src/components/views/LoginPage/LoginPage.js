a594d4604a01caf24f6342601cb87e7123d0d0d6
DESKTOP-TVPJ660\user <sekn1997@naver.com>
2021-08-25 18:20:46 +0900

    로그인 (프론트엔드)

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useDispatch } from "react-redux";
+import { loginUser } from '../../../_actions/user_action';
 
-function LoginPage() {
+
+function LoginPage(props) {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const dispatch = useDispatch();
+
+    const onChange = (event) => {
+        const {
+            target: { name, value }
+        } = event;
+        if (name === "email") {
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+
+        const body = {
+            email,
+            password
+        }
+
+        dispatch(loginUser(body))
+            .then(response => {
+                if (response.payload.loginSuccess) {
+                    props.history.push("/");
+                } else {
+                    alert("Error");
+                }
+            })
+    }
+
     return (
-        <div>
-            LoginPage
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center"
+            , width: "100%", height: "100vh"
+        }}>
+            <form style={{
+                display: "flex", flexDirection: "column"
+            }} onSubmit={onSubmit} >
+                <label>Email</label>
+                <input name="email" type="email" value={email} onChange={onChange} />
+                <label>Password</label>
+                <input name="password" type="password" value={password} onChange={onChange} />
+                <br />
+                <button type="submit">로그인</button>
+            </form>
         </div>
     )
 }
